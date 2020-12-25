268153e130d1494153db7025238a51c0b75b9dc2
tr7zw <tr7zw@live.de>
2020-12-25 01:56:30 +0100

    Clean probably unused method

--- src/main/java/de/tr7zw/nethermode/mixin/PlayerManagerMixin.java
+++ src/main/java/de/tr7zw/nethermode/mixin/PlayerManagerMixin.java
@@ -1,24 +1,15 @@
 package de.tr7zw.nethermode.mixin;
 
-import java.util.Optional;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.network.ClientConnection;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
 
 @Mixin(PlayerManager.class)
 public abstract class PlayerManagerMixin {
@@ -29,24 +20,6 @@ public abstract class PlayerManagerMixin {
 	@Redirect(method = "createPlayer", at = @At(value = "INVOKE", target = "net/minecraft/server/MinecraftServer.getOverworld()Lnet/minecraft/server/world/ServerWorld;"))
 	protected ServerWorld replaceStartWorld(MinecraftServer server) {
 		return server.getWorld(World.NETHER);
-	}
-
-	private Optional<?> lastSpawnPos = Optional.empty();
-	private ServerPlayerEntity lastPlayer = null;
-
-	@Inject(method = "respawnPlayer", at = @At(value = "HEAD"))
-	public ServerPlayerEntity respawnPlayer(ServerPlayerEntity player, boolean bl,
-			CallbackInfoReturnable<ServerPlayerEntity> info) {
-		lastPlayer = player;
-		BlockPos blockPos = player.getSpawnPointPosition();
-		boolean bl2 = player.isSpawnPointSet();
-		if (blockPos != null) {
-			lastSpawnPos = PlayerEntity.findRespawnPosition(this.server.getWorld(player.getSpawnPointDimension()),
-					blockPos, player.getSpawnAngle(), bl2, bl);
-		} else {
-			lastSpawnPos = Optional.empty();
-		}
-		return null;
 	}
 	
 	@Redirect(method = "onPlayerConnect", at = @At(value = "FIELD", target = "Lnet/minecraft/world/World;OVERWORLD:Lnet/minecraft/util/registry/RegistryKey;"))
